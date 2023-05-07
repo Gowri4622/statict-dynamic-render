c036372c7d861f57964b9892f14f30e5212fced0
Gowri4622 <gowrihifi@gmail.com>
2023-05-07 20:42:18 +0530

    Changes done in Service page.

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app=express()
+
+const about = require('./aboutPage.js')
+const service = require('./servicesPage.js')
+const branch = require('./branchPage.js')
+const home = require('./homePage.js')
+const contact = require('./contactPage.js')
+const post = require('./postPage.js')
+
+
+
+app.set('view engine','ejs')
+app.use(express.urlencoded({extended : true}))
+
+app.get('/',(request,response)=>{
+    // response.render('index',{text:'world'})
+    response.render('index')
+})
+
+
+app.use('/about',about)
+app.use('/service',service)
+app.use('/branch',branch)
+app.use('/home',home)
+app.use('/contact',contact)
+app.use('/post',post)
+
+app.listen(3600)
